5ed76b73cb36f10ec879b6095802caa84199de88
unknown <shebasaji1998@gmail.com git config --global user.namesheba>
2021-07-30 01:17:03 +0530

    using map and filter

--- js/conn.js
+++ js/conn.js
@@ -5,27 +5,42 @@ function station()
    fetch('http://52.203.100.234:5010/files/stops.txt')
   .then(response => response.json())
   .then(data => {
-    const stations= data;
-    var myJSON= JSON.stringify(stations,null,4);
-    document.getElementById('myStation').innerHTML=myJSON; 
-      
-  })
-
-}
+          
+           let {stop_id,stop_name, wheelchair_boarding,stop_lat,stop_long}= data;
+           console.log("data");
+            let stations = data.map((data) =>({ station_id:data.stop_id ,
+                                                station_name:data.stop_name ,
+                                                location: {
+                                                  longitude: data.stop_lon,
+                                                  latitude: data.stop_lat },
+                                                 wheelchairSuitable: data.wheelchair_boarding  
+                                              }));
+          let myJSON= JSON.stringify(stations,null,4);
+          document.getElementById('myStation').innerHTML=myJSON;
+           })
+        }
+    
 function price()
 {
    console.log("functioncalled2");
    fetch('http://52.203.100.234:5010/files/fare_attributes.txt')
   .then(response => response.json())
   .then(data => {
-    const fare= data;
-    var myJSON= JSON.stringify(fare,null,4);
-    document.getElementById('myPrice').innerHTML=myJSON; 
-      
-  })
+
+    let fares =data.filter (function(datas) {
+      return  datas.price;
+    })
+    .map((datas)=>({price: datas.price}));
+  
+  
+     let myJSON= JSON.stringify(fares,null,4);
+    document.getElementById('myPrice').innerHTML= myJSON; 
+})
+  
 }
 
 
+            
 
 
 
