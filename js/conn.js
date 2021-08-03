3db492a98dba27627a58b2451d2fa377a0d35f42
unknown <shebasaji1998@gmail.com git config --global user.namesheba>
2021-08-03 23:42:11 +0530

    2 data fetching

--- js/conn.js
+++ js/conn.js
@@ -1,47 +1,55 @@
-
-function station()
-{   
-   console.log("functioncalled1");
-   fetch('http://52.203.100.234:5010/files/stops.txt')
-  .then(response => response.json())
-  .then(data => {
-          
-           let {stop_id,stop_name, wheelchair_boarding,stop_lat,stop_long}= data;
-           console.log("data");
-            let stations = data.map((data) =>({ station_id:data.stop_id ,
-                                                station_name:data.stop_name ,
-                                                location: {
-                                                  longitude: data.stop_lon,
-                                                  latitude: data.stop_lat },
-                                                 wheelchairSuitable: data.wheelchair_boarding  
-                                              }));
-          let myJSON= JSON.stringify(stations,null,4);
-          document.getElementById('myStation').innerHTML=myJSON;
-           })
-        }
-    
-function price()
-{
-   console.log("functioncalled2");
-   fetch('http://52.203.100.234:5010/files/fare_attributes.txt')
-  .then(response => response.json())
-  .then(data => {
-
-    let fares =data.filter (function(datas) {
-      return  datas.price;
-    })
-    .map((datas)=>({price: datas.price}));
-  
-  
-     let myJSON= JSON.stringify(fares,null,4);
-    document.getElementById('myPrice').innerHTML= myJSON; 
-})
-  
+function station() {
+  console.log("functioncalled1");
+  fetch("http://52.203.100.234:5010/files/stops.txt")
+    .then((response) => response.json())
+    .then((data) => {
+      let { stop_id, stop_name, wheelchair_boarding, stop_lat, stop_long } =
+        data;
+      console.log("data");
+      let stations = data.map((data) => ({
+        station_id: data.stop_id,
+        station_name: data.stop_name,
+        location: {
+          longitude: data.stop_lon,
+          latitude: data.stop_lat,
+        },
+        wheelchairSuitable: data.wheelchair_boarding,
+      }));
+      let myJSON = JSON.stringify(stations, null, 4);
+      document.getElementById("myStation").innerHTML = myJSON;
+    });
 }
 
+function price() {
+  fetch("http://52.203.100.234:5010/files/fare_rules.txt")
+    .then((response) => response.json())
+    .then((data) => {
+      let { fare_id, origin_id, destination_id } = data;
+      let fare = data.map((data) => ({
+        fare_id: data.fare_id,
+        origin_id: data.origin_id,
+        destid: data.destination_id,
+      }));
+      console.log("yes");
+      fetch("http://52.203.100.234:5010/files/fare_attributes.txt")
+        .then((response) => response.json())
+        .then((value) => {
+          let { fare_id, price } = value;
+          let arr = {};
+          let fare_1 = value.filter(function (values) {
+            if (fare.fare_id == value.fare_id) {
+              arr = {
+                originid: fare.origin_id,
+                destinationid: fare.destination_id,
+                price: values.price,
+              };
+            }
 
-            
-
-
-
+            return arr;
+          });
+          let myJSON = JSON.stringify(fare_1, null, 4);
+          document.getElementById("myPrice").innerHTML = myJSON;
+        });
+    });
+}
 
